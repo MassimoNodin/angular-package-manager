ee428a15daa316d2bf40ec244fcab32e598d1d4a
MassimoNodin <massimonodin@gmail.com>
2024-10-01 23:30:18 +1000

    Created new components for driver and package management, navbar, footer, index and statistics page

---
+++ assignment-3/src/app/list-driver/list-driver.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-driver',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-driver.component.html',
+  styleUrl: './list-driver.component.css'
+})
+export class ListDriverComponent {
+
+}
---
+++ assignment-3/src/app/list-package/list-package.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-package',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-package.component.html',
+  styleUrl: './list-package.component.css'
+})
+export class ListPackageComponent {
+
+}
