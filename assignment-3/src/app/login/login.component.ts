8481eb73dd3b4e8f87b707cf0c22ea2a158aa03f
MassimoNodin <massimonodin@gmail.com>
2024-10-07 00:50:21 +1100

    Added html page and frontend logic for login form submission

--- assignment-3/src/app/login/login.component.ts
+++ assignment-3/src/app/login/login.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { DatabaseService } from '../database.service';
+import { firstValueFrom } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  constructor(private databaseService: DatabaseService) {}
 
+  router = inject(Router);
+
+  loginInformation = {
+    username: '',
+    password: ''
+  };
+
+  login() {
+    firstValueFrom(this.databaseService.login(this.loginInformation.username, this.loginInformation.password))
+      .then((response: any) => {
+        console.log(response.status);
+        if (response.status == 200) {
+          this.router.navigate(['/home']);
+          return;
+        }
+        else {
+          this.router.navigate(['/invalid']);
+        }
+      })
+      .catch((error: any) => {
+        this.router.navigate(['/invalid']);
+      });
+  }
 }
