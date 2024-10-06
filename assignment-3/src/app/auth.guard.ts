100f6416e1e9c71ff223b2ade630af2a45dcd32c
MassimoNodin <massimonodin@gmail.com>
2024-10-06 23:59:11 +1100

    Added autentication check for all endpoints which require logging in

---
+++ assignment-3/src/app/auth.guard.ts
@@ -0,0 +1,26 @@
+import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { DatabaseService } from './database.service';
+import { lastValueFrom } from 'rxjs';
+
+export const authGuard: CanActivateFn = async (route, state) => {
+  const authService = inject(DatabaseService);
+  const router = inject(Router);
+
+  try {
+    const isAuthenticated = await lastValueFrom(authService.isAuthenticated()); // Convert observable to promise
+    console.log('isAuthenticated', isAuthenticated);
+    if (isAuthenticated === true) {
+      return true;
+    } else {
+      router.navigate(['/login']);
+      return false;
+    }
+  } catch (error) {
+    // Handle any potential error (for example, network issues or authentication errors)
+    console.error('Error during authentication guard', error);
+    router.navigate(['/login']);
+    return false;
+  }
+};
