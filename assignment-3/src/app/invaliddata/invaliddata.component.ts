c1f81500042ff69d1c705d8018037eec6224abb1
MassimoNodin <massimonodin@gmail.com>
2024-10-06 16:02:55 +1100

    Created invalid data component

---
+++ assignment-3/src/app/invaliddata/invaliddata.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-invaliddata',
+  standalone: true,
+  imports: [],
+  templateUrl: './invaliddata.component.html',
+  styleUrl: './invaliddata.component.css'
+})
+export class InvaliddataComponent {
+
+}
