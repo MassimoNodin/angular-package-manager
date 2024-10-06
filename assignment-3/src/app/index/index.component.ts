69352d248a98e96690f92800512ddfb7068bd8e1
MassimoNodin <massimonodin@gmail.com>
2024-10-07 00:00:01 +1100

    Added logic to fix amount of drivers and packages to homepage

--- assignment-3/src/app/index/index.component.ts
+++ assignment-3/src/app/index/index.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DatabaseService } from '../database.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-index',
   standalone: true,
   imports: [],
   templateUrl: './index.component.html',
-  styleUrl: './index.component.css'
+  styleUrls: ['./index.component.css'] // Fixed typo from 'styleUrl' to 'styleUrls'
 })
-export class IndexComponent {
+export class IndexComponent implements OnInit {
+  driverAmount!: any;
+  packageAmount!: any;
+  
+  constructor(private databaseService: DatabaseService) {}
 
+  ngOnInit(): void {
+    this.databaseService.getDriverPackages().subscribe({
+      next: (data) => {
+        if (data && typeof data === 'object' && 'drivers' in data && 'packages' in data) {
+          this.driverAmount = data.drivers;
+          this.packageAmount = data.packages;
+        } else {
+          this.driverAmount = 0;
+          this.packageAmount = 0;
+        }
+      },
+      error: (error: HttpErrorResponse) => {
+        this.driverAmount = 0;
+        this.packageAmount = 0;
+      }
+    });
+  }
 }
