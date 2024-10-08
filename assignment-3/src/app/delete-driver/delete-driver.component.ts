5c1bd3a15c8331b4092e68f05bd778a3afd2fe57
MassimoNodin <massimonodin@gmail.com>
2024-10-08 18:29:40 +1100

    Added delete driver page

--- assignment-3/src/app/delete-driver/delete-driver.component.ts
+++ assignment-3/src/app/delete-driver/delete-driver.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { DatabaseService } from '../database.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-delete-driver',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './delete-driver.component.html',
   styleUrl: './delete-driver.component.css'
 })
 export class DeleteDriverComponent {
+  drivers: any = [];
+  loading: boolean = true;
+  driver_id: number = 0;
+  router: Router = inject(Router);
 
+  constructor(private databaseService: DatabaseService) {}
+
+  ngOnInit(): void {
+    this.databaseService.getDrivers().subscribe((data) => {
+      this.drivers = data;
+      this.loading = false;
+    });
+  }
+
+  deleteDriver() {
+    this.databaseService.deleteDriver(this.driver_id).subscribe((response) => {
+      this.router.navigate(['/drivers']);
+    });
+  }
 }
