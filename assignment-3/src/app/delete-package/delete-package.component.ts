7203dc2ac1b4144ddef81f3b67c8b5a8daf8db28
MassimoNodin <massimonodin@gmail.com>
2024-10-08 18:34:29 +1100

    Added package deletion to angular component and fixed driver addition

--- assignment-3/src/app/delete-package/delete-package.component.ts
+++ assignment-3/src/app/delete-package/delete-package.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { DatabaseService } from '../database.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-delete-package',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './delete-package.component.html',
   styleUrl: './delete-package.component.css'
 })
 export class DeletePackageComponent {
+  packages: any = [];
+  loading: boolean = true;
+  package_id: number = 0;
+  router: Router = inject(Router);
 
+  constructor(private databaseService: DatabaseService) {}
+
+  ngOnInit(): void {
+    this.databaseService.getPackages().subscribe((data) => {
+      this.packages = data;
+      this.loading = false;
+    });
+  }
+
+  deletePackage() {
+    this.databaseService.deletePackage(this.package_id).subscribe((response) => {
+      this.router.navigate(['/packages']);
+    });
+  }
 }
