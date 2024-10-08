019ad57ec22fe2a68c2aa93e401f066edbb0a488
MassimoNodin <massimonodin@gmail.com>
2024-10-08 16:44:42 +1100

    Added driver creation page

--- assignment-3/src/app/add-driver/add-driver.component.ts
+++ assignment-3/src/app/add-driver/add-driver.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { DatabaseService } from '../database.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-driver',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-driver.component.html',
   styleUrl: './add-driver.component.css'
 })
 export class AddDriverComponent {
+  constructor(private databaseService: DatabaseService) {}
 
+  router = inject(Router);
+
+  driverInformation = {
+    driver_name: '',
+    driver_department: '',
+    driver_licence: '',
+    driver_isActive: false
+  };
+
+  addDriver() {
+    this.databaseService.addDriver(this.driverInformation).subscribe((response: any) => {
+      console.log(response)
+      if (!response.error) {
+        this.router.navigate(['/']);
+        return;
+      }
+      else {
+        this.router.navigate(['/invalid']);
+      }
+    });
+  }
 }
